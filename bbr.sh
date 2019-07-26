0c4478e4baafc0c1668633f51fe53f88438a563e
xiaoz <xiaoz93@outlook.com>
2019-07-26 13:57:33 +0800

    add bbr

---
+++ bbr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##########	CentOS 7安装BBR		###############
+
+yum -y install wget
+wget https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm --import RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+
+#升级最新内核
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+#设置最新内核
+grub2-set-default 0
+
+#写入配置文件
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
+#重启服务器
+reboot
